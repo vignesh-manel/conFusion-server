c6bc2187cf4569de2aa73f9cdc1d78b27c99da0b
vmanel <vnayak83@gmail.com>
2019-08-04 15:54:14 +0000

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion'
+    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion',
+    'facebook': {
+        clientId: '631357740720338',
+        clientSecret: '846517dbc4d861fd4e4c4aa34746ae96'
+    }
 }
